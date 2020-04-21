6bea1957955c1bbe5f32783a7dfa22610501fc18
Andrew Williams <48336115+a-hungry-dev@users.noreply.github.com>
2020-04-21 20:45:25 +0100

    Update scripts.js

--- app/public/scripts.js
+++ app/public/scripts.js
@@ -1,13 +1,19 @@
-document.querySelector('button[type="submit"]').addEventListener('click', () => {
-   fetch('/fibonacci')
-      .then(res => res.json())
-      .then(data =>
-         data.forEach(goodness => {
-            document.body.innerHTML += `
-            <ul>
-               <li> ${goodness.body} </li>
-            </ul>
-            `;
-         })
-      );
+const button = document.querySelector('button[type="submit"]');
+const input = document.querySelector('input[type="number"]');
+const result = document.querySelector('.result');
+const error = document.querySelector('.error');
+button.addEventListener('click', () => {
+  // reset the error message when resubmitting request
+  error.innerHTML = ""
+
+  // use value from input to get nth number
+  fetch('/fibonacci?number=' + input.value)
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        error.innerHTML = data.error
+        return
+      }
+      result.innerHTML = data.result
+    });
 });
